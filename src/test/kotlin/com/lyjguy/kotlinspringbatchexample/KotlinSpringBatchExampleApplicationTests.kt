5f639299bfc4af75625e54ca08ed7cc54997ee12
lyjguy <lyjguy@naver.com>
2022-10-27 21:44:07 +0900

    init

---
+++ src/test/kotlin/com/lyjguy/kotlinspringbatchexample/KotlinSpringBatchExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lyjguy.kotlinspringbatchexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBatchExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
