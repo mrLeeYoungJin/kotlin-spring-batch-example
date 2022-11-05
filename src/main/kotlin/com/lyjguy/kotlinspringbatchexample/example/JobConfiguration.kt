018fd0e52576b9ed2df9ca151d8273faf91ce7c6
lyjguy <lyjguy@naver.com>
2022-11-05 11:52:50 +0900

    JobConfiguration batch 추가

---
+++ src/main/kotlin/com/lyjguy/kotlinspringbatchexample/example/JobConfiguration.kt
@@ -0,0 +1,45 @@
+package com.lyjguy.kotlinspringbatchexample.example
+
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.StepContribution
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.scope.context.ChunkContext
+import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class JobConfiguration(
+    private val jobBuilderFactory: JobBuilderFactory,
+    private val stepBuilderFactory: StepBuilderFactory,
+) {
+    @Bean
+    fun job(): Job {
+        return jobBuilderFactory.get("Job")
+            .start(step1())
+            .next(step2())
+            .build()
+    }
+
+    @Bean
+    fun step1(): Step {
+        return stepBuilderFactory.get("step1")
+            .tasklet { contribution, chunkContext ->
+                println("step1 has executed")
+                RepeatStatus.FINISHED
+            }
+            .build()
+    }
+
+    @Bean
+    fun step2(): Step {
+        return stepBuilderFactory.get("step2")
+            .tasklet { contribution: StepContribution?, chunkContext: ChunkContext? ->
+                println("step2 has executed")
+                RepeatStatus.FINISHED
+            }
+            .build()
+    }
+}
