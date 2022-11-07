4ef5a20ba6bdb408c3d2e566a495aed91c2be967
lyjguy <lyjguy@naver.com>
2022-11-07 21:24:55 +0900

    part3.1.2 JobResultListener 추가

---
+++ src/main/kotlin/com/lyjguy/kotlinspringbatchexample/example/part3_1_2/JobResultListener.kt
@@ -0,0 +1,40 @@
+package com.lyjguy.kotlinspringbatchexample.example.part3_1_2
+
+import org.springframework.batch.core.JobExecution
+import org.springframework.batch.core.JobExecutionListener
+
+class JobResultListener : JobExecutionListener {
+    override fun beforeJob(jobExecution: JobExecution) {}
+    override fun afterJob(jobExecution: JobExecution) {
+        println("")
+        println("=================================== Job name : " + jobExecution.jobInstance.jobName + " ===============================================")
+        println("jobExecution.getJobInstance().getInstanceId() : " + jobExecution.jobInstance.instanceId)
+        println("jobExecution.getStatus() : " + jobExecution.status)
+        println("jobExecution.getExitStatus() : " + jobExecution.exitStatus)
+        println("jobExecution.getCreateTime() : " + jobExecution.createTime)
+        println("jobExecution.getStartTime() : " + jobExecution.startTime)
+        println("jobExecution.getEndTime() : " + jobExecution.endTime)
+        println("jobExecution.getExecutionContext : " + jobExecution.executionContext)
+        println("jobExecution.getJobParameters().getParameters() : " + jobExecution.jobParameters.parameters)
+        val stepExecutions = jobExecution.stepExecutions
+        for (stepExecution in stepExecutions) {
+            println("")
+            println("================================= Step name : " + stepExecution.stepName + " =====================================================")
+            println("stepExecution.getStatus() : " + stepExecution.status)
+            println("stepExecution.getExitStatus() : " + stepExecution.exitStatus)
+            println("stepExecution.getCommitCount() : " + stepExecution.commitCount)
+            println("stepExecution.getRollbackCount() : " + stepExecution.rollbackCount)
+            println("stepExecution.getReadCount() : " + stepExecution.readCount)
+            println("stepExecution.getReadSkipCount() : " + stepExecution.readSkipCount)
+            println("stepExecution.getStartTime() : " + stepExecution.startTime)
+            println("stepExecution.getEndTime() : " + stepExecution.endTime)
+            println("stepExecution.getWriteCount() : " + stepExecution.writeCount)
+            println("stepExecution.getWriteSkipCount() : " + stepExecution.writeSkipCount)
+            println("stepExecution.getFilterCount() : " + stepExecution.filterCount)
+            println("stepExecution.getProcessSkipCount() : " + stepExecution.processSkipCount)
+            println("stepExecution.getSkipCount() : " + stepExecution.skipCount)
+            println("stepExecution.getExecutionContext() : " + stepExecution.executionContext)
+        }
+        println("")
+    }
+}
