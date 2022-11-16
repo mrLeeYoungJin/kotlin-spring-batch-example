6312a63e7c4ffb5827818a660f0572b78f199c0b
lyjguy <lyjguy@naver.com>
2022-11-16 13:43:11 +0900

    part3.2.2 StepExecutionConfiguration

---
+++ src/main/kotlin/com/lyjguy/kotlinspringbatchexample/example/part3_2_2/StepExecutionConfiguration.kt
@@ -0,0 +1,62 @@
+package com.lyjguy.kotlinspringbatchexample.example.part3_2_2
+
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.StepContribution
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.scope.context.ChunkContext
+import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class StepExecutionConfiguration {
+    private val jobBuilderFactory: JobBuilderFactory? = null
+    private val stepBuilderFactory: StepBuilderFactory? = null
+    @Bean
+    fun BatchJob(): Job {
+        return jobBuilderFactory!!["Job"]
+            .start(step1())
+            .next(step2())
+            .build()
+    }
+
+    @Bean
+    fun step1(): Step {
+        return stepBuilderFactory!!["step1"]
+            .tasklet { contribution, chunkContext ->
+                println("step1 has executed")
+                RepeatStatus.FINISHED
+            }
+            .build()
+    }
+
+    @Bean
+    fun step2(): Step {
+        return stepBuilderFactory!!["step2"]
+            .tasklet { contribution: StepContribution?, chunkContext: ChunkContext? ->
+                println("step2 has executed")
+                RepeatStatus.FINISHED
+            }
+            .build()
+    } /* @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(executionContextTasklet1)
+                .build();
+    }
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet(executionContextTasklet2)
+                .build();
+    }
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet(executionContextTasklet3)
+                .build();
+    }*/
+}
