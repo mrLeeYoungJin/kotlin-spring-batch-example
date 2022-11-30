8a5fcbfa33aab372c09282dcd1d34bb0dcc62ec2
lyjguy <lyjguy@naver.com>
2022-11-30 13:47:38 +0900

    part4.2.2.2 StartNextConfiguration

---
+++ src/main/kotlin/com/lyjguy/kotlinspringbatchexample/example/part4_2_2_2/StartNextConfiguration.kt
@@ -0,0 +1,55 @@
+package com.lyjguy.kotlinspringbatchexample.example.part4_2_2_2
+
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.StepContribution
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.scope.context.ChunkContext
+import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class StartNextConfiguration {
+    private val jobBuilderFactory: JobBuilderFactory? = null
+    private val stepBuilderFactory: StepBuilderFactory? = null
+    @Bean
+    fun batchJob(): Job {
+        return jobBuilderFactory!!["batchJob"]
+            .start(step1())
+            .next(step2())
+            .next(step3())
+            .build()
+    }
+
+    @Bean
+    fun step1(): Step {
+        return stepBuilderFactory!!["step1"]
+            .tasklet { contribution: StepContribution?, chunkContext: ChunkContext? ->
+                println("step1 has executed")
+                RepeatStatus.FINISHED
+            }
+            .build()
+    }
+
+    @Bean
+    fun step2(): Step {
+        return stepBuilderFactory!!["step2"]
+            .tasklet { contribution: StepContribution?, chunkContext: ChunkContext? ->
+                println("step2 has executed")
+                RepeatStatus.FINISHED
+            }
+            .build()
+    }
+
+    @Bean
+    fun step3(): Step {
+        return stepBuilderFactory!!["step3"]
+            .tasklet { contribution: StepContribution?, chunkContext: ChunkContext? ->
+                println("step3 has executed")
+                RepeatStatus.FINISHED
+            }
+            .build()
+    }
+}
