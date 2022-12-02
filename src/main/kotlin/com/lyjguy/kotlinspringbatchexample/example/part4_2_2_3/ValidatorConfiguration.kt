8223ab7218f6ef70a0d66bca303d05e3dd96a9e3
lyjguy <lyjguy@naver.com>
2022-12-02 13:18:51 +0900

    part4.2.2.3 ValidatorConfiguration

---
+++ src/main/kotlin/com/lyjguy/kotlinspringbatchexample/example/part4_2_2_3/ValidatorConfiguration.kt
@@ -0,0 +1,56 @@
+package com.lyjguy.kotlinspringbatchexample.example.part4_2_2_3
+
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.StepContribution
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.scope.context.ChunkContext
+import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ValidatorConfiguration {
+    private val jobBuilderFactory: JobBuilderFactory? = null
+    private val stepBuilderFactory: StepBuilderFactory? = null
+    @Bean
+    fun batchJob(): Job {
+        return jobBuilderFactory!!["batchJob"]
+            .validator(CustomJobParametersValidator()) //                .validator(new DefaultJobParametersValidator(new String[]{"name"},new String[]{"year"}))
+            .start(step1())
+            .next(step2())
+            .next(step3())
+            .build()
+    }
+
+    @Bean
+    fun step1(): Step {
+        return stepBuilderFactory!!["step1"]
+            .tasklet { contribution: StepContribution?, chunkContext: ChunkContext? ->
+                println("step1 has executed")
+                RepeatStatus.FINISHED
+            }
+            .build()
+    }
+
+    @Bean
+    fun step2(): Step {
+        return stepBuilderFactory!!["step2"]
+            .tasklet { contribution: StepContribution?, chunkContext: ChunkContext? ->
+                println("step2 has executed")
+                RepeatStatus.FINISHED
+            }
+            .build()
+    }
+
+    @Bean
+    fun step3(): Step {
+        return stepBuilderFactory!!["step3"]
+            .tasklet { contribution: StepContribution?, chunkContext: ChunkContext? ->
+                println("step3 has executed")
+                RepeatStatus.FINISHED
+            }
+            .build()
+    }
+}
