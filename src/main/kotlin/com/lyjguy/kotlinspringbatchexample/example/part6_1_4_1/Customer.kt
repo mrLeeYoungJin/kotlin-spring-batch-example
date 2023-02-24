fcd48bb4b3d77103cbd3b87e367afed065ff3edc
lyjguy <lyjguy@naver.com>
2023-02-24 17:09:05 +0900

    part6.1.4.1. Customer

---
+++ src/main/kotlin/com/lyjguy/kotlinspringbatchexample/example/part6_1_4_1/Customer.kt
@@ -0,0 +1,8 @@
+package com.lyjguy.kotlinspringbatchexample.example.part6_1_4_1
+
+data class Customer(
+    val id: Long,
+    val firstName: String,
+    val lastName: String,
+    val birthdate: String,
+)
