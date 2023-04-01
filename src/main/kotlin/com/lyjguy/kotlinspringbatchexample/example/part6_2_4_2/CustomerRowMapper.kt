0c959c3cc13d049bafef57d9379638459862310f
lyjguy <lyjguy@naver.com>
2023-04-01 12:08:20 +0900

    part6.2.4.2 CustomerRowMapper

---
+++ src/main/kotlin/com/lyjguy/kotlinspringbatchexample/example/part6_2_4_2/CustomerRowMapper.kt
@@ -0,0 +1,18 @@
+package com.lyjguy.kotlinspringbatchexample.example.part6_2_4_2
+
+import org.springframework.jdbc.core.RowMapper
+import java.sql.ResultSet
+import java.sql.SQLException
+
+class CustomerRowMapper : RowMapper<Customer> {
+
+    @Throws(SQLException::class)
+    override fun mapRow(rs: ResultSet, i: Int): Customer {
+        return Customer(
+            rs.getLong("id"),
+            rs.getString("firstName"),
+            rs.getString("lastName"),
+            rs.getString("birthdate")
+        )
+    }
+}
